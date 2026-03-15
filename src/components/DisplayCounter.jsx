9f574546d4901a4f3639ad336f0a046ee953fde1
Azkka azeem <azkaazeem804@gmail.com>
2026-03-15 15:58:51 +0500

    Something went wrong

--- src/components/DisplayCounter.jsx
+++ src/components/DisplayCounter.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const DisplayCounter = () => {
+    const counter = useSelector(store => store.counter);
     return (
         <>
             <p className="lead mb-4">
-                Counter Current value
+                Counter Current value: {counter}
             </p>
         </>
     )
