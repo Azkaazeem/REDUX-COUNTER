bfacaed494501213b8532a0fad6deac05f955366
Azkka azeem <azkaazeem804@gmail.com>
2026-03-15 15:44:12 +0500

    Continue after sometimes

---
+++ src/components/DisplayCounter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DisplayCounter = () => {
+    return (
+        <>
+            <p className="lead mb-4">
+                Counter Current value
+            </p>
+        </>
+    )
+}
+
+export default DisplayCounter
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import {createStore} from "redux";
+
+const INITIAL_VALUE = {
+    counter: 0
+}
+
+const counterReducer = ( store , INITIAL_VALUE , action ) => {
+    console.log(action);
+    return store;
+}
+
+const counterStore = createStore(counterReducer)
+
+
+export default counterStore;
